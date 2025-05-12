d5b2bcbd08abb84e98b8f9422873154fb0daeaaf
agusgrynszpan <agrynszpan@frba.utn.edu.ar>
2025-05-12 04:32:15 -0300

    Agregar salida redireccionada y verificar hola mundo

--- 00-CHelloworld/hello.c
+++ 00-CHelloworld/hello.c
@@ -3,8 +3,18 @@
 
 int main(){
 
-printf("hola,mundo!\c");
+printf("Hola,mundo!\n");
 
 return 0;
 
 }
+
+/* Comandos utilizados para verificar la salida y redireccionarla:
+PS C:\Users\agusg\OneDrive\Escritorio\Sintaxis\ssl> cd .\00-CHelloworld
+PS C:\Users\agusg\OneDrive\Escritorio\Sintaxis\ssl\00-CHelloworld> gcc -std=c17 hello.c -o hola
+PS C:\Users\agusg\OneDrive\Escritorio\Sintaxis\ssl\00-CHelloworld> .\hola.exe
+Hola,mundo!
+PS C:\Users\agusg\OneDrive\Escritorio\Sintaxis\ssl\00-CHelloworld> .\hola.exe > output.txt
+PS C:\Users\agusg\OneDrive\Escritorio\Sintaxis\ssl\00-CHelloworld> type output.txt
+Hola,mundo!
+*/
